1163440bb81b01c98172373ee229de24914bc920
Gabouchet <gabriel.hamel@epitech.eu>
2019-09-26 20:30:11 +0200

    Added lib boost

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -5,7 +5,20 @@
 ** main
 */
 
+#include <boost/asio.hpp>
+#include <iostream>
+
 int main(int ac, char **av)
 {
+	boost::asio::io_service ios;
+	boost::asio::ip::tcp::resolver resolver(ios);
+	boost::asio::ip::tcp::resolver::query query("www.developpez.com", "80");
+	boost::asio::ip::tcp::resolver::iterator iter = resolver.resolve(query);
+	boost::asio::ip::tcp::resolver::iterator end;
+
+	while (iter != end) {
+		boost::asio::ip::tcp::endpoint endpoint = *iter++;
+		std::cout << endpoint << std::endl;
+	}
     return 0;
 }
