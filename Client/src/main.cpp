a3638a7bcb235131437fc5c2b3f06be33a788b70
Gabouchet <gabriel.hamel@epitech.eu>
2019-09-26 11:59:48 +0200

    Project architecture

---
+++ Client/src/main.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2019
+** CPP_babel_2019
+** File description:
+** main
+*/
+
+int main(int ac, char **av)
+{
+    return 0;
+}
---
+++ Server/include/example.hpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2019
+** CPP_babel_2019
+** File description:
+** example
+*/
+
+#pragma once
