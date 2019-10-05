1685adb9d905ab6029aa88504504b0b126e34435
Gabouchet <gabriel.hamel@epitech.eu>
2019-10-05 23:45:21 +0200

    Arg on servre

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -14,10 +14,14 @@ using namespace bbl::srv;
 
 int main(int ac, char **av)
 {
+    if (ac != 3) {
+        std::cerr << "Usage:\n\t" << av[0] << " <storage> <port>" << std::endl;
+        return 84;
+    }
     try {
-        IStorage *database = new SqlClient("storage.sql");
+        IStorage *database = new SqlClient(av[1]);
         NetworkManager service(database);
-        std::unique_ptr<INetworkServer> server(new BoostNetworkServer(8080, service));
+        std::unique_ptr<INetworkServer> server(new BoostNetworkServer(std::stoi(av[2]), service));
         server->run();
         delete database;
     }
